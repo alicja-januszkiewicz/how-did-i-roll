2af2ddb1de971b0f72461d241a1c46c53a744678
Alicja Januszkiewicz <d.januszkiewicz@gmail.com>
2023-10-31 01:37:40 +0000

    fix Dioxus.toml

--- docs/assets/dioxus/snippets/dioxus-interpreter-js-603636eeca72cf05/inline0.js
+++ docs/assets/dioxus/snippets/dioxus-interpreter-js-603636eeca72cf05/inline0.js
@@ -163,19 +163,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    let s = "";let lsp,sp,sl; let c = new TextDecoder();const attr = [];
-                    let attr_tmp1, attr_tmp2;
-                    function get_attr() {
-                        attr_tmp2 = u8buf[u8bufp++];
-                        if(attr_tmp2 & 128){
-                            attr_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
-                            attr[attr_tmp2&4294967167]=attr_tmp1;
-                            return attr_tmp1;
-                        }
-                        else{
-                            return attr[attr_tmp2&4294967167];
-                        }
-                    }let u32buf,u32bufp;let u8buf,u8bufp;const evt = [];
+    const evt = [];
                     let evt_tmp1, evt_tmp2;
                     function get_evt() {
                         evt_tmp2 = u8buf[u8bufp++];
@@ -187,7 +175,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return evt[evt_tmp2&4294967167];
                         }
-                    }const ns_cache = [];
+                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();const ns_cache = [];
                     let ns_cache_tmp1, ns_cache_tmp2;
                     function get_ns_cache() {
                         ns_cache_tmp2 = u8buf[u8bufp++];
@@ -199,8 +187,20 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
+                    }let u32buf,u32bufp;let u8buf,u8bufp;const attr = [];
+                    let attr_tmp1, attr_tmp2;
+                    function get_attr() {
+                        attr_tmp2 = u8buf[u8bufp++];
+                        if(attr_tmp2 & 128){
+                            attr_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
+                            attr[attr_tmp2&4294967167]=attr_tmp1;
+                            return attr_tmp1;
+                        }
+                        else{
+                            return attr[attr_tmp2&4294967167];
+                        }
                     }
-            let value,ns,field,ptr,event_name,len,id,bubbles;
+            let ns,ptr,bubbles,value,event_name,len,id,field;
             export function create(r){
                 d=r;
             }
